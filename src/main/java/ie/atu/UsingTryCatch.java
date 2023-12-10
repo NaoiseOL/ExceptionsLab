65d75d138618a7ca3a22a03fcb839e53db83b805
naoise <G00385498@atu.ie>
2023-12-10 22:10:17 +0000

    Made changes to deal with non number values using try catch

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -1,21 +1,31 @@
 package ie.atu;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class UsingTryCatch {
 
     public static void main(String[] args){
-        System.out.println("Please enter a number between 0-9 : ");
-        Scanner myscan = new Scanner(System.in);
+        int x = 0;
 
-        String input = myscan.nextLine();
 
-        int myNum = Integer.parseInt(input);
+        while(x<1) {
+            System.out.println("Please enter a number between 0-9: ");
+            Scanner myScan = new Scanner(System.in);
+            try {
+                String input = myScan.nextLine();
 
-        if(myNum >= 0 && myNum <= 9){
-            System.out.println("You entered " + myNum);
-        }
-        else{
-            System.out.println("Not a valid number");
+                int myNum = Integer.parseInt(input);
+
+                if (myNum >= 0 && myNum <= 9) {
+                    System.out.println("You Entered " + myNum);
+                    x++;
+                } else {
+                    System.out.println("Not a valid Input");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Not a valid Number value");
+
+            }
         }
 
     }
